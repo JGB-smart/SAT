3c9a69d4b8f0e1e621f285e654afefd2e3dfd003
Juan <juanegalindezberlioz@gmail.com>
2023-05-07 19:22:08 -0400

    antes de cambiar table perfil

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,6 +3,7 @@ from usuarios import views
 
 
 urlpatterns = [
-    path('lista_usuarios/', views.Usuarios, name='lista_usuarios'),
+    path('lista_usuarios/', views.ListadoUsuarios.as_view(), name='lista_usuarios'),
+    path('lista_usuarios_grupos/', views.ListadoUsuario_Grupo.as_view(), name='lista_usuarios_grupos'),
     # path('usuarios/', views.ListadoUsuarios.as_view(), name='usuarios'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView,ListView, CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.models import User
+from perfil.models import Perfil
+
+class ListadoUsuarios(ListView):
+    model = User
+    template_name = 'lista_usuarios.html'
+    context_object_name = 'usuarios'             # permite cambiar de nombre al objetos listview que se llamara en la vista
+    queryset=  User.objects.all()           #  Permite realizar filtros a la consulta
+    
+    @method_decorator(login_required)             #Puede agrupar decoradores
+    def dispatch(self, request, *args, **kwargs):
+        # if not request.user.has_perm('productos.view_productos'):
+        #       messages.success(request,"ACCESO DENEGADO")
+        #       return redirect('home')
+        #print(Perfil.objects.select_related('user').query)
+        #print(User.objects.prefetch_related('username').query)
+        # print(Group.users.all())
+
+        return super( ListadoUsuarios,self).dispatch(request, *args, **kwargs)
+    
+
+    
+class ListadoUsuario_Grupo(ListView):
+    model = User
+    template_name = 'lista_usuarios_grupos.html'
+    context_object_name = 'usuarios_grupos'             # permite cambiar de nombre al objetos listview que se llamara en la vista
+    queryset=  User.objects.all().values('username',"groups__name", "perfil__puesto")           #  Permite realizar filtros a la consulta
+    
+    @method_decorator(login_required)             #Puede agrupar decoradores
+    def dispatch(self, request, *args, **kwargs):
+        # if not request.user.has_perm('productos.view_productos'):
+        #       messages.success(request,"ACCESO DENEGADO")
+        #       return redirect('home')
+        #print(Perfil.objects.select_related('user').query)
+        #print(User.objects.prefetch_related('username').query)
+        # print(Group.users.all())
+
+        return super( ListadoUsuario_Grupo,self).dispatch(request, *args, **kwargs)
+    
 
 
-def Usuarios(request):
-
-
-    return render(request,'lista_usuarios.html')
